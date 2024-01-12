4b8f78b2d1185b729a6ee7e1c7908abeb2effb42
MIKU-7437 <nurshanov.dias@gmail.com>
2024-01-12 21:07:39 +0600

    daphne fix 6

--- gen_zone/gen_zone/asgi.py
+++ gen_zone/gen_zone/asgi.py
@@ -1,17 +1,19 @@
 import os
+import django
+from django.core.asgi import get_asgi_application
+django_asgi_app = get_asgi_application()
 
 from chat.middlewares import WebSocketJWTAuthMiddleware
+
 from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
 from chat import routing
-from channels.security.websocket import AllowedHostsOriginValidator
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gen_zone.settings')
-
+django.setup()
 
 application = ProtocolTypeRouter(
     {
-        "http": get_asgi_application(),
+        "http": django_asgi_app,
         "websocket": WebSocketJWTAuthMiddleware(URLRouter(routing.websocket_urlpatterns)),
     }
 )
